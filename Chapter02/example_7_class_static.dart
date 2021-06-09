c0820cfe315854633acfbe365606d00eddfcaf4c
Wen <wenslife@outlook.com>
2021-06-09 23:32:40 +0800

    static method

--- Chapter02/example_7_class_static.dart
+++ Chapter02/example_7_class_static.dart
@@ -6,6 +6,10 @@ class Person {
   Person(this.firstName, this.lastName);
 
   String get fullName => "$personLabel $firstName $lastName";
+
+  static void printsPerson(Person person) {
+    print("$personLabel ${person.firstName} ${person.lastName}");
+  }
 }
 
 void main() {
@@ -14,9 +18,13 @@ void main() {
 
   print(somePerson.fullName); // prints Person name: Wen Liang
   print(anotherPerson.fullName); // prints Person name: Kana Hanazawa
+  Person.printsPerson(somePerson); // prints Person name: Wen Liang
+  Person.printsPerson(anotherPerson); // prints Person name: Kana Hanazawa
 
   Person.personLabel = "name:";
 
   print(somePerson.fullName); // prints name: Wen Liang
   print(anotherPerson.fullName); // prints name: Kana Hanazawa
+  Person.printsPerson(somePerson); // prints Person name: Wen Liang
+  Person.printsPerson(anotherPerson); // prints Person name: Kana Hanazawa
 }
