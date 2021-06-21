f5aea4e3f0d1c9decaedf4b904a3685e168cc742
Wen <wenslife@outlook.com>
2021-06-21 21:52:10 +0800

    make Friend fields required

--- Chapter04/hands_on_layouts/lib/friend.dart
+++ Chapter04/hands_on_layouts/lib/friend.dart
@@ -1,11 +1,11 @@
 class Friend {
-  final String? name;
-  final String? number;
-  final String? photoURL;
+  final String name;
+  final String number;
+  final String photoURL;
 
   Friend({
-    this.name,
-    this.number,
-    this.photoURL,
+    required this.name,
+    required this.number,
+    required this.photoURL,
   });
 }
